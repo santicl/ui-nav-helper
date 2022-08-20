33436e82ab01c6464ec12782ee27e36dad92e041
santiago <caballerosuarezsantiago@gmail.com>
2022-08-20 11:18:51 -0500

    change name NavHelper

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import { Nav } from "./nav-component.js";
+import { NavHelper } from "./nav-component.js";
 let URL = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
 function App() {
-    Nav('app', {
+    NavHelper('app', {
         urlImg: [URL],
-        lists: ['Home', 'About', 'Contact', 'Portfolio'],
+        lists: [ 'Home', 'About', 'Contact', 'Portfolio' ],
         ALT: 'Google'
     });
 }
--- src/nav-component.js
+++ src/nav-component.js
@@ -1,9 +1,9 @@
-export function Nav(ID = null, { urlImg = [], lists = [], ALT = String } = {}) {
+export function NavHelper(ID = null, { urlImg = null, lists = [], ALT = String } = {}) {
     document.getElementById(ID)
         .appendChild(createNav(lists, urlImg, ALT));
 }
 
-function createNav(lists, urlImg, ALT) {
+function createNav({ lists, urlImg, ALT } = {}) {
     let navElement = document.createElement('nav');
     navElement.setAttribute('class', 'nav');
     navElement.appendChild(createTagImg(urlImg, ALT));
@@ -25,7 +25,7 @@ function createTagUl(LI) {
     return ulElement;
 }
 
-function createTagImg(urlImg, ALT) {
+function createTagImg({ urlImg, ALT } = {}) {
     let imgElement = document.createElement('img');
     imgElement.setAttribute('src', urlImg);
     imgElement.setAttribute('alt', ALT);
