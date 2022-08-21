816a6c2966f92fee0099a4d82be99ec0d654daa6
santiago <caballerosuarezsantiago@gmail.com>
2022-08-20 20:22:43 -0500

    restructuring and add ui-gradients component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
+/**
+ * 
+ * @param 
+ * ID name by element into DOM
+ * { urlImg = null, lists = [], color = '', styles = '', ALT = String } = {}
+ * urlImg = url image
+ * lists = array of object { tag, href }
+ * colors = color of background with gradients
+ * styles = class name of element
+ * ALT = alt of image
+ * 
+ * @return Navigation with styles equal to styles the normal Nav
+ * 
+ * **/
+// example:
 import { NavHelper } from "./nav-component.js";
 let URL = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
 
     NavHelper('app', {
-        urlImg: URL,
-        lists: [ 
+        urlImg: URL, // url image
+        lists: [  // array of object { tag, href } for TAG A element
             { tag: 'Home', href: 'index.html' }, 
-            { tag: 'About', href: 'index.html' }, 
-            { tag: 'Contact', href: 'index.html' } 
+            { tag: 'About', href: 'about.html' }, 
+            { tag: 'Contact', href: 'contact.html' } 
         ],
-        ALT: 'Google'
+        // styles = 'normal' or 'ui-gradients';
+        styles: 'ui-gradients',
+        colors: ['#d1b7b7', '#f7f1e3'], // colors of background with gradients
+        ALT: 'Google' // alt of image
     });
 
 window.onload = NavHelper;
--- src/nav-component.js
+++ src/nav-component.js
@@ -1,15 +1,30 @@
-export function NavHelper(ID = null, { urlImg = null, lists = [], ALT = String } = {}) {
-    console.log(lists)
-    document.getElementById(ID)
-        .appendChild(createNav(lists, urlImg, ALT));
+/**
+ * 
+ * @param 
+ * ID name by element into DOM
+ * { urlImg = null, lists = [], color = '', styles = '', ALT = String } = {}
+ * urlImg = url image
+ * lists = array of object { tag, href }
+ * color = color of background
+ * styles = class name of element
+ * ALT = alt of image
+ * 
+ */
+
+export function NavHelper(ID = null, { urlImg = null, lists = [], colors = [ length = 2 ], styles = '', ALT = String } = {}) {
+    let dataArguments = { urlImg, lists, colors, styles, ALT };
+    document.getElementById(ID).appendChild(createNav(dataArguments));
 }
 
-function createNav( lists, urlImg, ALT ) {
+function createNav(data) {
+    let { urlImg, lists, colors, styles, ALT } = data;
     let navElement = document.createElement('nav');
-    navElement.setAttribute('class', 'nav');
+    let className = 'normal' ? 'nav' : styles;
+    navElement.setAttribute('class', className);
     navElement.appendChild(createTagImg(urlImg, ALT));
     navElement.appendChild(createTagUl(lists));
     navElement.appendChild(createTagI());
+    navElement.style.background = styles == 'normal' ? '#d1b7b7' : `linear-gradient(to right, ${colors[0]}, ${colors[1]})`;
     return navElement;
 }
 
