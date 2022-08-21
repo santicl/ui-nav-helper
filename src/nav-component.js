865f507bb5936e718b25c3f53d6c13e46018977a
santiago <caballerosuarezsantiago@gmail.com>
2022-08-20 21:51:40 -0500

    add err-control

--- src/nav-component.js
+++ src/nav-component.js
@@ -12,8 +12,8 @@
  */
 
 export function NavHelper(ID = null, { urlImg = null, lists = [], colors = [length = 2], styles = '', ALT = String } = {}) {
-    let dataArguments = { urlImg, lists, colors, styles, ALT };
     try {
+        let dataArguments = { urlImg, lists, colors, styles, ALT };
         document.getElementById(ID).appendChild(createNav(dataArguments));
     } catch (err) {
         return console.log(err);
